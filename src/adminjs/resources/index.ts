4d04f66414b6f2427afbf9e69f647ccd2a30a95c
José Carlos <jose-carlos64@live.com>
2024-07-15 23:37:10 -0300

    Tabela de usuários com senha criptografada e seeder admin

--- src/adminjs/resources/index.ts
+++ src/adminjs/resources/index.ts
@@ -1,8 +1,9 @@
 import { ResourceWithOptions } from "adminjs";
-import { Category, Course, Episode } from "../../models";
+import { Category, Course, Episode, User } from "../../models";
 import { categoryResourceOptions } from "./Category";
 import { courseResourceFeatures, courseResourceOptions } from "./Course";
 import { episodeResourceFeatures, episodeResourceOptions } from "./Episode";
+import { userResourceOptions } from "./User";
 
 export const adminJsResourcesOptions: ResourceWithOptions[] = [
     {
@@ -19,4 +20,8 @@ export const adminJsResourcesOptions: ResourceWithOptions[] = [
         options: episodeResourceOptions,
         features: episodeResourceFeatures
     },
+    {
+        resource: User,
+        options: userResourceOptions
+    }
 ]
