6f578b4ce4c1b24009374b08331b2dba57e42e6b
José Carlos <jose-carlos64@live.com>
2024-07-25 22:57:46 -0300

    Adicionando Favoritos

--- src/models/index.ts
+++ src/models/index.ts
@@ -2,16 +2,28 @@ import { Category } from "./Category";
 import { Course } from "./Course";
 import { Episode } from "./Episode";
 import { User } from "./User";
+import { Favorite } from "./Favorite";
 
+// Categoria - Curso
 Category.hasMany(Course, { as: 'courses' });
 Course.belongsTo(Category);
 
+// Curso - Episódio
 Course.hasMany(Episode, { as: 'episodes' });
 Episode.belongsTo(Course);
+
+// Usuário - Favorito - Curso
+Course.belongsToMany(User, { through: Favorite });
+User.belongsToMany(Course, { through: Favorite });
+Course.hasMany(Favorite, { as: 'FavoritesUsers', foreignKey: 'course_id'});
+Favorite.belongsTo(Course);
+User.hasMany(Favorite, { as: 'FavoritesCourses', foreignKey: 'user_id'});
+Favorite.belongsTo(User);
 
 export {
     Category,
     Course,
     Episode,
-    User
+    User,
+    Favorite,
 }
