33aa109da80d965a75ea1435ad8a90b309963480
José Carlos <jose-carlos64@live.com>
2024-07-29 18:52:10 -0300

    atualizando informações do usuário

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -14,6 +14,28 @@ export const usersController = {
             }
         }
     },
+    // PUT /users/current
+    update: async (req: AuthenticatedRequest, res: Response) => {
+        
+        const { firstName, lastName, phone, birth, email } = req.body;
+
+        try {
+            const { id } = req.user!;
+            const updatedUser = await userService.update(id, {
+                firstName,
+                lastName,
+                phone,
+                birth,
+                email,
+            });
+
+            return res.json(updatedUser);
+        } catch (error) {
+            if(error instanceof Error) {
+                return res.status(400).json({ message: error.message });
+            }
+        }
+    },
     //GET /users/current/watching
     watching: async (req: AuthenticatedRequest, res: Response) => {
         const { id } = req.user!;
